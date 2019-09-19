fa551c69df8236c6d9a446559f2cdc8b76e32904
Octopussyzz <spudd.otw@gmail.com>
2019-09-19 22:51:37 +0200

    landing front added but its not really working

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 // styles
 import "./assets/css/bootstrap.min.css";
+import "./assets/css/paper-kit.css"
 import "perfect-scrollbar/css/perfect-scrollbar.css";
 // pages
 
